a4b73722f9e5fb736f89cb51f43468f63071d61c
mivashchenko.work <mivashchenko.work@gmail.com>
2025-03-19 13:37:25 -0400

    initial 3

---
+++ src/app/_components/component.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export const Component = () => {
+  return (
+    <div>
+      <h1>Component</h1>
+
+      222 {process.env.NEXT_PUBLIC_API_URL} 222
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import {Component} from "@/app/_components/component";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        222 {process.env.NEXT_PUBLIC_API_URL} 222
+
+
+        <Component/>
       </main>
     </div>
   );
