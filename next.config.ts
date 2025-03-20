ec2557aa696315fec94f009c49e2296acfa85f41
mivashchenko.work <mivashchenko.work@gmail.com>
2025-03-19 21:10:49 -0400

    initial 9

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // This will be available in the browser
-  },
 };
 
 export default nextConfig;
--- src/app/_components/component.tsx
+++ src/app/_components/component.tsx
@@ -7,7 +7,7 @@ export const Component = () => {
     <div>
       <h1>Component</h1>
 
-     888 {process.env.NEXT_PUBLIC_API_URL} 888
+     999 {process.env.NEXT_PUBLIC_API_URL} 999
     </div>
   )
 }
