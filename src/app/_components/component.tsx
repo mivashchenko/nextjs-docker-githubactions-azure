7e833231251248c76c07e7febed63ab57b80cbef
mivashchenko.work <mivashchenko.work@gmail.com>
2025-03-19 21:34:20 -0400

    initial 10

--- src/app/_components/component.tsx
+++ src/app/_components/component.tsx
@@ -7,7 +7,7 @@ export const Component = () => {
     <div>
       <h1>Component</h1>
 
-     999 {process.env.NEXT_PUBLIC_API_URL} 999
+     10 {process.env.NEXT_PUBLIC_API_URL} 10
     </div>
   )
 }
